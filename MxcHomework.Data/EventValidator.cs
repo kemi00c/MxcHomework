dfb168b6c53a656063906ce55bbf945a85d7a393
Adam Kemenes <kemenes.adam1@gmail.com>
2025-09-13 11:43:28 +0200

    Added event validator with tests

---
+++ MxcHomework.Data/EventValidator.cs
@@ -0,0 +1,42 @@
+﻿using MxcHomework.Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MxcHomework.Data
+{
+    public class EventValidator
+    {
+        private Event _event;
+
+        public EventValidator(Event e)
+        {
+            _event = e;
+        }
+
+        public void Validate()
+        {
+            if (string.IsNullOrEmpty(_event.Name))
+            {
+                throw new EventValidatorException("Mandatory field, Name is empty.");
+            }
+
+            if (string.IsNullOrEmpty(_event.Location))
+            {
+                throw new EventValidatorException("Mandatory field, Location is empty");
+            }
+
+            if (_event.Location.Length > 100)
+            {
+                throw new EventValidatorException("Location field is longer than the maximum allowed 100 characters.");
+            }
+
+            if (_event.Capacity != null && _event.Capacity <= 0)
+            {
+                throw new EventValidatorException("Capacity should be a positive value.");
+            }
+        }
+    }
+}
---
+++ MxcHomework.Data/EventValidatorException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MxcHomework.Data
+{
+    public class EventValidatorException : Exception
+    {
+        public EventValidatorException(string message) : base(message)
+        {
+        }
+    }
+}
